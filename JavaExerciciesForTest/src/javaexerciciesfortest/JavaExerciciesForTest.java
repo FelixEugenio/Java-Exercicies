b45881f1e75552becbd20ffac2659acb7825e68c
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-03 20:58:19 +0100

    configure initial Structure

---
+++ JavaExerciciesForTest/src/javaexerciciesfortest/JavaExerciciesForTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaexerciciesfortest;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class JavaExerciciesForTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
