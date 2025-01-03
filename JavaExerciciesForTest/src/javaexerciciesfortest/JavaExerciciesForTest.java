7e4c32a3cf795c0ee35ea754b21d46fcddd09770
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-03 21:39:04 +0100

    pedindo os valores das posioes da matriz

--- JavaExerciciesForTest/src/javaexerciciesfortest/JavaExerciciesForTest.java
+++ JavaExerciciesForTest/src/javaexerciciesfortest/JavaExerciciesForTest.java
@@ -16,7 +16,26 @@ public class JavaExerciciesForTest {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][3]; //define a variável matriz como um array de 2 dimensões com 3*3 elementos
         
+        //Podemos preencher e imprimir todos os elementos da matriz com dois ciclos encadeados 
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print("Digite o valor para a posição [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+        
+        /*
+        for (int i = 0; i <matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = 1 + i * matriz[0].length + j;
+                System.out.print(matriz[i][j] + "\t");
+            }
+            
+            System.out.println("");
+        }
+*/
     }
-    
 }
